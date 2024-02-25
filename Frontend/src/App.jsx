431818747017407c2377a35c161e6a9497b86b39
HasanCoder66 <hasanashraf874@gmail.com>
2024-02-25 14:18:10 +0500

    nav and mobile nav working

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,48 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+// import Home from "./Pages/Home/Home";
+import { useState } from "react";
+import Nav from "./Components/Navbar/Nav";
+import MobileNav from "./Components/MobileNav/MobileNav";
+
+
+const Layout = () => {
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
+  return (
+    <div className="app">
+      <MobileNav nav={nav} closeNav={closeNav} />
+      <Nav openNav={openNav} />
+      <Outlet />
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      // {
+      //   path: "/",
+      //   element: <Home />,
+      // },
+      // {
+      //   path: "/Home",
+      //   element: <Home />,
+      // },
+      
+    ],
+  },
+]);
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
-export default App
+export default App;
