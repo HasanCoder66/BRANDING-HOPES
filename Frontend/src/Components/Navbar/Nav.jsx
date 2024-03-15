43588f0aa70237411b4c803d11edda79139dfe59
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-03-16 00:37:57 +0500

    nav changes

--- Frontend/src/Components/Navbar/Nav.jsx
+++ Frontend/src/Components/Navbar/Nav.jsx
@@ -9,10 +9,10 @@ const Nav = ({ openNav }) => {
   return (
     <>
       {/* <Topbar /> */}
-      <div className="w-[100%] fixed z-[10000]  h-[11vh] bg-[#14213d] shadow-md">
-        <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
+      <div className="w-[100%] fixed z-[10000] flex items-center  h-[11vh] bg-[#14213d] shadow-md">
+        <div className="w-[100%] flex items-center justify-between h-[100%] px-[50px]">
           <Link to="/">
-            <div className=" w-20 cursor-pointer  ">
+            <div className=" w-20 cursor-pointer">
               <img
                 src="https://res.cloudinary.com/dz25g2syh/image/upload/v1707806557/BrandingHopes/Icon-01_f4tz1r.png "
                 alt="Branding-Hopes "
@@ -20,34 +20,32 @@ const Nav = ({ openNav }) => {
               />
             </div>
           </Link>
-
-          <Link to="/">
-            <div className="nav-link">Home</div>
-          </Link>
-          <div className="nav-link">
-            <DropdownMenu />
+          <div className="flex gap-[19px] ">
+            <Link to="/">
+              <div className="nav-link">Home</div>
+            </Link>
+            <div className="nav-link">
+              <DropdownMenu />
+            </div>
+            <Link to="/about">
+              <div className="nav-link">About</div>
+            </Link>
+            <Link to='/portfolio'>
+              <div className="nav-link">Portfolio</div>
+            </Link>
+            <Link to="/blog">
+              <div className="nav-link">Blog</div>
+            </Link>
+            {/* <Link >
+              <div className="nav-link">Services</div>
+            </Link> */}
+            <Link to="/Contact">
+              <div className="nav-link">Contact</div>
+            </Link>
           </div>
-          <Link to="/about">
-            <div className="nav-link">About</div>
-          </Link>
-          <Link to="/blog">
-            <div className="nav-link">Blog</div>
-          </Link>
-          <Link >
-            <div className="nav-link">Services</div>
-          </Link>
-          <Link to='/portfolio'>
-            <div className="nav-link">Portfolio</div>
-          </Link>
-          <Link to="/Contact">
-            <div className="nav-link">Contact</div>
-          </Link>
-          <Link to="/GetConsultancy">
-            <div className="nav-link">Toll Free</div>
-          </Link>
-          <div onClick={openNav}>
-            <Bars3Icon className="w-[2rem] md:hidden h-[2rem]  text-[#fca311] cursor-pointer " />
-          </div>
+        </div>
+        <div onClick={openNav}>
+          <Bars3Icon className="w-[2rem] md:hidden h-[2rem]  text-[#fca311] cursor-pointer " />
         </div>
       </div>
     </>
