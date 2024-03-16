550a70e81a703c6c0e4cff72461f94bda4c78ff9
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-03-16 23:56:39 +0500

    hero and service section redesign

--- Frontend/src/Components/Hero/Landing.jsx
+++ Frontend/src/Components/Hero/Landing.jsx
@@ -5,15 +5,16 @@ import Button from "../ButtonNew/Button";
 const Landing = () => {
   return (
     // id='landing'
-    <div className="">
-      <div className=' bg-[url("https://res.cloudinary.com/dpvxkqhi8/image/upload/v1710503547/branding%20hopes/WhatsApp_Image_2024-03-15_at_16.38.38_afbfff7c_gf9sfy.jpg")] bg-cover bg-center  pt-[80px] pb-[40px]  '>
-        <div className="wrapper min-h-[90vh] p-[80px]  flex flex-col item-center justify-around text-center uppercase font-['Poppins', sans-serif'] ">
+    <div className='bg-[url("https://res.cloudinary.com/dpvxkqhi8/image/upload/v1710503547/branding%20hopes/WhatsApp_Image_2024-03-15_at_16.38.38_afbfff7c_gf9sfy.jpg")] bg-cover bg-center min-h-[100vh]'>
+      <div className="wrapper p-[80px]  flex lg:flex-row flex-col-reverse item-center justify-between text-center uppercase font-['Poppins', sans-serif'] mt-[26px]">
+        <div className="left flex-1 flex flex-col items-center justify-center gap-[30px]">
+          <p className="text-[1.3rem] font-normal text-[#e5e5e5]">Empower Your Presence with Branding Hopes</p>
           <div className="landingTitle text-8xl font-bold text-white">
             <span className="text-[#fca311]">Turning Your Hopes </span>Into
             Brands
           </div>
-          <p className="my-[50px] text-[1.3rem] font-normal text-[#e5e5e5]">
-            Empower Your Presence with Branding Hopes - Where Imagination Meets
+          <p className="text-[1.3rem] font-normal text-[#e5e5e5]">
+            Where Imagination Meets
             Innovation in Web Development, Design, SEO, and Digital Mastery
           </p>
           <div className="btns flex justify-center gap-[20px]">
@@ -21,8 +22,12 @@ const Landing = () => {
             <Button text="Our Services" link="/services" />
           </div>
         </div>
+        <div className="right flex-1 flex items-center justify-center">
+          <div className="imgContainer w-[600px]">
+            <img src="/Innovation-amico.svg" alt="" className="w-[100%] object-contain" />
+          </div>
+        </div>
       </div>
-      {" "}
     </div>
   );
 };
--- Frontend/src/Components/PortfolioCard/PortCard.jsx
+++ Frontend/src/Components/PortfolioCard/PortCard.jsx
@@ -3,13 +3,11 @@ import React from 'react'
 const PortCard = ({ data }) => {
   // console.log(data, "======>>>> data for port")
   return (
-    <div className=''>
-      <div className='w-[300px] h-[300px] rounded flex flex-col items-center gap-[5px] cursor-pointer my-[10px] mt-[6rem]'>
+    <div className='w-[300px] h-[300px] rounded flex flex-col items-center justify-center gap-[10px] cursor-pointer my-[10px]'>
       <div className='w-[100%] h-[100%] rounded'>
         <img src={data?.imgUrl} alt="" className='w-[100%] h-[100%] object-cover rounded' />
       </div>
-      <p className='text-[#fca311] font-bold'>{data?.title}</p>
-    </div>
+      <p className='text-[#14213d] font-bold'>{data?.title}</p>
     </div>
   )
 }
