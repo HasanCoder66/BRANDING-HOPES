5d130fa8e13228939a31fd634a90f6e2cde6268f
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-03-18 00:10:18 +0500

    framer motion on landing & service sec

--- Frontend/src/Components/Hero/Landing.jsx
+++ Frontend/src/Components/Hero/Landing.jsx
@@ -1,31 +1,69 @@
 // import { Link } from 'react-router-dom'
 import "./landing.css";
 import Button from "../ButtonNew/Button";
+import { motion } from "framer-motion"
+import { useRef } from "react";
+
+
+const container = {
+  hidden: { opacity: 0, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.1,
+      staggerChildren: 0.2
+    }
+  }
+}
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+}
+
+const imgVariant = {
+  initial: {
+    x: 500,
+  },
+  animate: {
+    x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 260,
+      damping: 20
+    }
+  }
+}
 
 const Landing = () => {
+  const constraintsRef = useRef(null)
   return (
     // id='landing'
-    <div className='bg-[url("https://res.cloudinary.com/dpvxkqhi8/image/upload/v1710503547/branding%20hopes/WhatsApp_Image_2024-03-15_at_16.38.38_afbfff7c_gf9sfy.jpg")] bg-cover bg-center min-h-[100vh]'>
+    <div className='bg-[url("https://res.cloudinary.com/dpvxkqhi8/image/upload/v1710503547/branding%20hopes/WhatsApp_Image_2024-03-15_at_16.38.38_afbfff7c_gf9sfy.jpg")] bg-cover bg-center min-h-[100vh] overflow-x-hidden'>
       <div className="wrapper p-[80px]  flex lg:flex-row flex-col-reverse item-center justify-between text-center uppercase font-['Poppins', sans-serif'] mt-[26px]">
-        <div className="left flex-1 flex flex-col items-center justify-center gap-[30px]">
-          <p className="text-[1.3rem] font-normal text-[#e5e5e5]">Empower Your Presence with Branding Hopes</p>
-          <div className="landingTitle text-8xl font-bold text-white">
+        <motion.div variants={container} initial="hidden" animate="visible" className="left flex-1 flex flex-col items-center justify-center gap-[30px]">
+          <motion.p variants={item} className="text-[1.3rem] font-normal text-[#e5e5e5]">Empower Your Presence with Branding Hopes</motion.p>
+          <motion.div variants={item} className="landingTitle text-8xl font-bold text-white">
             <span className="text-[#fca311]">Turning Your Hopes </span>Into
             Brands
-          </div>
-          <p className="text-[1.3rem] font-normal text-[#e5e5e5]">
+          </motion.div>
+          <motion.p variants={item} className="text-[1.3rem] font-normal text-[#e5e5e5]">
             Where Imagination Meets
             Innovation in Web Development, Design, SEO, and Digital Mastery
-          </p>
-          <div className="btns flex justify-center gap-[20px]">
+          </motion.p>
+          <motion.div variants={item} className="btns flex justify-center gap-[20px]">
             <Button text="Get Started" link="/" />
             <Button text="Our Services" link="/services" />
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
         <div className="right flex-1 flex items-center justify-center">
-          <div className="imgContainer w-[600px]">
-            <img src="/Innovation-amico.svg" alt="" className="w-[100%] object-contain" />
-          </div>
+          <motion.div variants={imgVariant} initial="initial" animate="animate" className="imgContainer w-[600px]">
+            <motion.img src="/Innovation-amico.svg" alt="" className="w-[100%] object-contain" />
+          </motion.div>
         </div>
       </div>
     </div>
