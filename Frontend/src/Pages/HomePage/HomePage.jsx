ddc8b3c7bc1c8dac0a49fede2669dee188d79e4d
HasanCoder66 <hasanashraf874@gmail.com>
2024-03-20 01:43:54 +0500

    mobile nav img set

--- Frontend/src/Pages/HomePage/HomePage.jsx
+++ Frontend/src/Pages/HomePage/HomePage.jsx
@@ -4,7 +4,6 @@ import BlogPage from "../BlogPage/BlogPage";
 import ContactPage from "../ContactPage/ContactPage";
 import About from "../../Components/About/About";
 import ServiceSection from "../../Components/ServiceSection/ServiceSection";
-// import PortCard from "../../Components/PortfolioCard/PortCard";
 const HomePage = () => {
   return (
     <div className="homeContainer flex flex-col items-center">
@@ -13,13 +12,6 @@ const HomePage = () => {
       <div className="">
         <About />
       </div>
-      {/* <PricingCmp /> */}
-      {/* <div className="content flex items-center justify-evenly lg:flex-row md:flex-col sm:flex-col"> */}
-      {/* <CardPackages type="startup" color="bg-white" />
-        <CardPackages type="standard" color="bg-white" />
-        <CardPackages type="premium" color="bg-white" /> */}
-      {/* </div> */}
-      {/* <PortCard /> */}
       <BlogPage />
       <ContactPage />
     </div>
