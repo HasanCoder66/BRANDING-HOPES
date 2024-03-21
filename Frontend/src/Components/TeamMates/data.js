d05c65726cac56473157e9fb7c56a8eccf2f3463
HasanCoder66 <hasanashraf874@gmail.com>
2024-03-21 23:53:57 +0500

    data moving

--- Frontend/src/Components/TeamMates/data.js
+++ Frontend/src/Components/TeamMates/data.js
@@ -13,18 +13,18 @@ const TeamMateData = [
       Role: "Full Stack Developer and Founder",
       buttonContent: `view more about `,
     },
+    {
+       imgUrl: "https://scontent.fhdd3-1.fna.fbcdn.net/v/t39.30808-6/305535406_131917102916717_6182782624124545240_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=h1S_hdlWf-0AX800hxA&_nc_ht=scontent.fhdd3-1.fna&oh=00_AfCTQpbflZ2xFq2mom5eTSG3s2RprI1RvSLibKBMAgzpwg&oe=6600BA81",
+       name: "Abdul Majeed ",
+       city: "Karachi Pakistan",
+       Role: "Web Developer || Full stack Developer ",
+       buttonContent: `view more about `,
+     },
  {
     imgUrl: "https://scontent.fhdd3-1.fna.fbcdn.net/v/t39.30808-6/426701355_4621095901449668_5220658541142648378_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Ty4mwdCxMy0AX_HF3pV&_nc_ht=scontent.fhdd3-1.fna&oh=00_AfBANI87W2JTOFLFcRPR7Ou8TuS8NfVRyrHUBOql8oLDVw&oe=65FFE937",
     name: "Mukhtar Ahmed",
     city: "Karachi Pakistan",
     Role: "Graphic Designer ",
-    buttonContent: `view more about `,
-  },
- {
-    imgUrl: "https://scontent.fhdd3-1.fna.fbcdn.net/v/t39.30808-6/305535406_131917102916717_6182782624124545240_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=h1S_hdlWf-0AX800hxA&_nc_ht=scontent.fhdd3-1.fna&oh=00_AfCTQpbflZ2xFq2mom5eTSG3s2RprI1RvSLibKBMAgzpwg&oe=6600BA81",
-    name: "Abdul Majeed ",
-    city: "Karachi Pakistan",
-    Role: "Web Developer || Full stack Developer ",
     buttonContent: `view more about `,
   },
 ];
